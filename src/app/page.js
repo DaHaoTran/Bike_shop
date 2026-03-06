5110912f9300f281ebd9dc52bd1a57b263e1aa0a
DaHaoTran <nhao4771@gmail.com>
2026-03-06 22:25:42 +0700

    Add carousel section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import BikeType from "./components/bike_type";
+import CarouselReview from "./components/carousel_review";
 
 export default function Home() {
   return (
     <div>
+      {/* Carousel section */}
+      <div className={styles.carousel_container}>
+        <CarouselReview />
+      </div>
+      {/* End Carousel section */}
       {/* Types section */}
-      <div className="mt-5">
-        <h1 className="my-4 text-center">Khám phá sản phẩm</h1>
+      <div className="my-4">
+        <h1 className="my-4 text-center"><strong>Khám phá sản phẩm</strong></h1>
         <div className="row">
           {Array.from({ length: 5 }).map((_, index) => (
             <div className="col-lg-2 col-md-6 me-lg-4" key={index}>
