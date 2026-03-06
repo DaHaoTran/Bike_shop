5447be4c5385c32677ad8796e93807fe58663fd0
DaHaoTran <nhao4771@gmail.com>
2026-03-06 10:26:56 +0700

    Add new folder

---
+++ src/app/components/bike_type.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import image from '../../assets/images/sample.png'
+import Image from 'next/image'
+import styles from './bike_type.module.css'
+
+export default function BikeType() {
+  return (
+    <div className={styles.type_container}>
+        <div className={styles.type_background}></div>
+        <h4 className={styles.type_name}><strong>Type name</strong></h4>
+        <Image
+            className='position-relative'
+            src={image} 
+            width={350}
+            alt='bike image' />
+    </div>
+  )
+}
