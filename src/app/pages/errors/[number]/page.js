cc98329948613e4c1b31a354202ec80df840c8e6
DaHaoTran <nhao4771@gmail.com>
2026-03-07 21:05:05 +0700

    Add new folder

---
+++ src/app/pages/errors/[number]/page.js
@@ -0,0 +1,20 @@
+"use client"
+import React from 'react'
+import styles from './page.module.css'
+import { useParams, useSearchParams } from 'next/navigation'
+
+export default function Error() {
+  const params = useParams()
+
+  return (
+    <div className={styles.error_container}>
+      <div>
+        <h1 className={styles.error_number_label}>{params.number}</h1>
+        <div className={styles.error_button_container}>
+          <button className='px-4'>Reload</button>
+        </div>
+      </div>
+      <h5 className={styles.error_sub_title}>Something <br /> went wrong :(( <br /> ...</h5>
+    </div>
+  )
+}
