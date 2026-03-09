7a185a63f9fe1e295ab0dcc8b4d1028514eed0b6
DaHaoTran <nhao4771@gmail.com>
2026-03-09 21:36:55 +0700

    Add suspense component

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { Suspense } from 'react'
 import styles from './page.module.css'
 import Bike from '../components/bike'
 import { useSearchParams } from 'next/navigation'
@@ -9,15 +9,17 @@ export default function Products() {
     const searchStr = searchParams.get("str");
 
     return (
-        <div className={styles.products_container}>
-            <h1 className='mx-4 mb-4 fw-bold'>Kết quả tìm kiếm: {searchStr.includes('from') ? 'theo giá lọc': searchStr}</h1>
-            <div className='row'>
-                {Array.from({ length: 3 }).map((_, index) => (
-                    <div className='col-lg-4 col-md-6' key={index}>
-                        <div className='d-flex justify-content-center'><Bike /></div>
-                    </div>
-                ))}
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <div className={styles.products_container}>
+                <h1 className='mx-4 mb-4 fw-bold'>Kết quả tìm kiếm: {searchStr.includes('from') ? 'theo giá lọc' : searchStr}</h1>
+                <div className='row'>
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <div className='col-lg-4 col-md-6' key={index}>
+                            <div className='d-flex justify-content-center'><Bike /></div>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </Suspense>
     )
 }
