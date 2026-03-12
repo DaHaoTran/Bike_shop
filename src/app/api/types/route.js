9e4cf38a22df6fc0bb967cacb48d0ef745fc808b
DaHaoTran <nhao4771@gmail.com>
2026-03-12 07:13:17 +0700

    Fix status number api

--- src/app/api/types/route.js
+++ src/app/api/types/route.js
@@ -8,9 +8,9 @@ const getTypes = () => JSON.parse(fs.readFileSync(filePath, "utf-8"));
 export async function GET() {
     try {
         const types = getTypes();
-        if(!types) return NextResponse.json({message: 'Not found !'}, {status: "404"});
+        if(!types) return NextResponse.json({message: 'Not found !'}, {status: 404});
         return NextResponse.json(types);
     } catch (error) {
-        return NextResponse.json({message: error.message}, {status: "500"});
+        return NextResponse.json({message: error.message}, {status: 500});
     }
 }
