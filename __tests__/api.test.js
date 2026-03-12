b13f2f9d56a573a422b384e15d99306265cccc4f
DaHaoTran <nhao4771@gmail.com>
2026-03-12 07:25:44 +0700

    Add new file

---
+++ __tests__/api.test.js
@@ -0,0 +1,16 @@
+// setupTests.js
+require('jest-fetch-mock').enableMocks();
+
+describe('bikes api', () => {
+    it('get bikes', async () => {
+        const res = await fetch('api/bikes');
+        expect(res.ok).toBe(true);
+    })
+})
+
+describe('types api', () => {
+  it('get types', async () => {
+    const res = await fetch('api/types');
+    expect(res.ok).toBe(true)
+  })
+})
