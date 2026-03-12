0011c19bb0a0177cab0dd4dadc80f979ba5f4417
DaHaoTran <nhao4771@gmail.com>
2026-03-12 22:13:42 +0700

    Add new store

--- src/app/stores/store.js
+++ src/app/stores/store.js
@@ -2,11 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import typeReducer from '../features/type/type_slice'
 import bikeReducer from '../features/bike/bike_slice'
+import firmReducer from '../features/firm/firmSlice'
 
 var store = configureStore({
   reducer: {
     type: typeReducer,
     bike: bikeReducer,
+    firm: firmReducer,
   },
 });
 
