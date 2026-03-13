9115786876160473240cb24f07c6a359b61226d9
DaHaoTran <nhao4771@gmail.com>
2026-03-13 14:28:51 +0700

    Edit filter methods

--- src/app/api/bikes/filters/route.js
+++ src/app/api/bikes/filters/route.js
@@ -9,8 +9,15 @@ export async function GET(request) {
     try {
         const { searchParams } = new URL(request.url);
         const str = searchParams.get("str");
+        //get min and max value
+        const min = parseInt(str.slice(str.indexOf(m) + 1, str.indexOf('t')));
+        const max = parseInt(str.slice(str.indexOf(t) + 2));
+        //rest code
         const limit = searchParams.get("limit") == null ? 30 : searchParams.get("limit");
-        const bikes = getBikes().filter(x => str.includes(x.price.splice(0, x.price.indexOf('.'))) ||  str.includes(x.price.splice(x.price.indexOf('-') + 1, x.price.indexOf('.'))));
+        const bikes = getBikes().filter(x => 
+            parseInt(x.price.slice(0, x.price.indexOf('.'))) >= min &&
+            parseInt(x.price.slice(x.price.indexOf('-') + 2, x.price.indexOf('.'))) <= max
+        );
         if (!bikes) return NextResponse.json({ message: 'Not found' }, { status: 404 });
         return NextResponse.json(bikes.slice(0, limit));
     } catch (error) {
