80139c156384f6a70b8099a477f1efff02ef95c3
DaHaoTran <nhao4771@gmail.com>
2026-03-28 20:24:49 +0700

    Add new file

---
+++ src/app/components/chat_form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './chat_form.module.css'
+
+export default function ChatForm() {
+  return (
+    <>
+      <div className={styles.chat_form_container}>
+        <div className={styles.chat_form_header}><h3 className='text-center'>CHATBOT</h3></div>
+      </div>
+    </>
+  )
+}
