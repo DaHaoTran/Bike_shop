294148ee78b910a4da7b5857c6e4f66a3229f6ca
DaHaoTran <nhao4771@gmail.com>
2026-03-28 14:55:06 +0700

    Add run check

--- src/app/methods/list.js
+++ src/app/methods/list.js
@@ -7,29 +7,40 @@ import { useFetch } from "../hooks/useFetch";
 
 // const filePathDefault = path.join(process.cwd(), "src", "assets", "jsons");
 const fetcher = (...args) => fetch(...args).then(res => res.json());
+let firstUrl = ''
+
+const checkEnvironment = () => {
+    if(process.env.NODE_ENV !== 'development') {
+        firstUrl = 'https://dahaotran.github.io/Bike_shop'
+    }
+}
 
 const getTypeList = () => {
     // const filePath = path.join(filePathDefault, "types.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    return useSWR('https://dahaotran.github.io/Bike_shop/jsons/types.json', fetcher);
+    checkEnvironment();
+    return useSWR(firstUrl + '/jsons/types.json', fetcher);
 }
 
 const getBikeList = () => {
     // const filePath = path.join(filePathDefault, "bikes.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    return useSWR('https://dahaotran.github.io/Bike_shop/jsons/bikes.json', fetcher);
+    checkEnvironment();
+    return useSWR(firstUrl + '/jsons/bikes.json', fetcher);
 }
 
 const getFirmList = () => {
     // const filePath = path.join(filePathDefault, "firms.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    return useSWR('https://dahaotran.github.io/Bike_shop/jsons/firms.json', fetcher);
+    checkEnvironment();
+    return useSWR(firstUrl + '/jsons/firms.json', fetcher);
 }
 
 const getDetailList = () => {
     // const filePath = path.join(filePathDefault, "bike_details.json");
     // return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-    return useSWR('https://dahaotran.github.io/Bike_shop/jsons/bike_details.json', fetcher);
+    checkEnvironment();
+    return useSWR(firstUrl + '/jsons/bike_details.json', fetcher);
 }
 
 export { getTypeList, getBikeList, getFirmList, getDetailList };
